193aa4b09009b4ea62f24be62316cf4279a558cf
YoannPelamie <pelamieyoann@gmail.com>
2024-07-16 00:04:54 -0400

    exercice 2 - requis  4 refactoring pour 5

--- greeting.js
+++ greeting.js
@@ -7,8 +7,13 @@ function greet(name) {
         return `HELLO ${name}`;
     }
 
-    if (Array.isArray(name) && name.length === 2) {
-        return `Hello, ${name[0]} and ${name[1]}`;
+    if (Array.isArray(name)) {
+        if (name.length === 2) {
+            return `Hello, ${name[0]} and ${name[1]}`;
+        } else if (name.length >= 3) {
+            let lastElement = name.pop(); // Remove and get the last element
+            return `Hello, ${name.join(', ')}, and ${lastElement}`;
+        }
     }
     return `Hello, ${name}`;
 }
